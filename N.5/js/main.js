ccdb5ea3f35a3d830ae68959f5c99810f2d6e691
Junayed78 <86018097+Junayed78@users.noreply.github.com>
2021-07-28 22:55:48 +0600

    Add files via upload

---
+++ N.5/js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+ 
+ jQuery('#main_menu').meanmenu({
+  meanMenuContainer:'#mobile_menu',
+  meanScreenWidth:480,
+});
+
+
+
+
+
+
+
+
+  //MIXITUP(PORTFOULIO)
+ var mixer = mixitup('.content');
+});
